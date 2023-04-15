551a6d1e99381698177443ad85d6bdab9bf351aa
Lisha Ballard <ldom3976@Lishas-Air.askey.com>
2023-04-15 11:46:30 -0400

    added code for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+console.log('Putting data into the database.');
+const jateData = await openDB('jate', 1);
+const tx = jateData.transaction('jate', 'readwrite');
+const objectStr = tx.objectStore('jate');
+const req = objectStr.put({ id: 1, value: content });
+const result = await req;
+console.log('Data saved to the database.', result);
+};
+
+//console.error('putDb not implemented');
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.log('Getting data from the database');
+const jateData = await openDB('jate', 1);
+const tx = jateData.transaction('jate', 'readonly');
+const objectStr = tx.objectStore('jate');
+const req = objectStr.get(1);
+const result = await req; 
+console.log('Data saved to the database.', result);
+};
+
+//console.error('getDb not implemented');
 
 initdb();
